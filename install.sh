307f1c935046341537c929c3003255b92eb3e661
Chirag Sakhuja <dev.sakhuja@gmail.com>
2016-03-24 00:16:03 -0500

    Remove existing config

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-rm -rf .tmux*
-rm -rf .vim*
+rm -rf ~/.tmux*
+rm -rf ~/.vim*
+rm ~/.tmux.conf
+rm ~/.tmux.colors
+rm ~/.vimrc
 
 ln -s .tmux.conf ~/.tmux.conf
 ln -s .tmux.colors ~/.tmux.colors
 ln -s .vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle
-cd ~/.vim/bundle/YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe.vim
+cd ~/.vim/bundle/YouCompleteMe.vim
 git submodule --init --recursive
 ./install.sh --clang-completer
 sed -i '/compat/d' third_party/ycmd/cpp/ycm
