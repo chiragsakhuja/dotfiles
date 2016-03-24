59f87262042909720c88702b5a979c5c08a455f9
Chirag Sakhuja <dev.sakhuja@gmail.com>
2016-03-24 00:42:03 -0500

    Install script works correctly now

--- install.sh
+++ install.sh
@@ -6,17 +6,17 @@ rm ~/.tmux.conf
 rm ~/.tmux.colors
 rm ~/.vimrc
 
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .tmux.colors ~/.tmux.colors
-ln -s .vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.colors ~/.tmux.colors
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe.vim
-cd ~/.vim/bundle/YouCompleteMe.vim
-git submodule --init --recursive
-./install.sh --clang-completer
-sed -i '/compat/d' third_party/ycmd/cpp/ycm
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer
+sed -i.bak '/compat/d' ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
 cd -
 
 vim -S "install_commands.ex"
