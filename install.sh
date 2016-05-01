df0dd68022da5caa26d34b042515b2ce5b18f202
Chirag Sakhuja <dev.sakhuja@gmail.com>
2016-04-30 21:53:26 -0500

    Remove YCM from install script

--- install.sh
+++ install.sh
@@ -12,11 +12,5 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer
-sed -i.bak '/compat/d' ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
-cd -
 
 vim -S "install_commands.ex"
